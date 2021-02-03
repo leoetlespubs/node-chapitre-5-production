55f35330daa297494bf08427bd8651ab67fda58d
leo <leoetlespubs@gmail.com>
2021-02-03 16:00:29 +0100

    first commit

---
+++ database/index.js
@@ -0,0 +1,6 @@
+const mongoose = require('mongoose');
+const env = require(`../environment/${ process.env.NODE_ENV }`);
+
+mongoose.connect(env.dbUrl, { 
+  useNewUrlParser: true
+}).then( () => console.log('connexion db ok !')).catch( err => console.log(err));
