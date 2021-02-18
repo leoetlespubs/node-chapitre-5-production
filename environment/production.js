8b5a20769dbefcb4e2f9ad806e527ea712e10536
leo <leoetlespubs@gmail.com>
2021-02-18 16:36:54 +0100

    fix

--- environment/production.js
+++ environment/production.js
@@ -1,7 +1,6 @@
-const path = require('path');
 
 module.exports = {
     dbUrl: 'mongodb+srv://alex:qwe@cluster0-l4izx.gcp.mongodb.net/twitter?retryWrites=true',
-    cert: path.join( __dirname, ''),
-    key: path.join( __dirname, '')
+    cert: '/etc/letsencrypt/live/nodejstest.ddns.net/fullchain.pem',
+    key: '/etc/letsencrypt/live/nodejstest.ddns.net/privkey.pem'
 }
